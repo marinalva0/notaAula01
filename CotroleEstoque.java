c4fe229fbe77493b32387c65488bfde1dc287486
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-11-18 14:27:20 -0300

    Add files via upload

---
+++ CotroleEstoque.java
@@ -0,0 +1,11 @@
+public class CotroleEstoque extends Produtos{
+private String cadastros;
+
+
+public void setCadastros(String cadastros){
+    this.cadastros = cadastros;
+}
+public String getCadastros(){
+    return cadastros;
+}
+}
